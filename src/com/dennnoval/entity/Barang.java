ca47092c91ec8a1f228eaa9deba9c3fe029930c7
Noval Akbar <dennnoval@gmail.com>
2021-05-13 21:51:30 +0700

    Added barang entity object

---
+++ src/com/dennnoval/entity/Barang.java
@@ -0,0 +1,37 @@
+package com.dennnoval.entity;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author dennnoval
+ */
+public class Barang implements Serializable {
+  private final String kode;
+  private final String nama;
+  private final int harga;
+  private final int stok;
+
+  public Barang(String kode, String nama, int harga, int stok) {
+    this.kode = kode;
+    this.nama = nama;
+    this.harga = harga;
+    this.stok = stok;
+  }
+
+  public String getKode() {
+    return kode;
+  }
+
+  public String getNama() {
+    return nama;
+  }
+
+  public int getHarga() {
+    return harga;
+  }
+  
+  public int getStok() {
+    return harga;
+  }
+}
